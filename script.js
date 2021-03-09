0c29fa12935fb9d3f686f247e946c2dccc6eb3a3
Hanna Kozak <hannawkozak@gmail.com>
2021-03-09 22:41:53 +0000

    mobile first design

---
+++ script.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const navigation_icon = document.querySelector(".navigation__icon");
+const icon_close = document.querySelector(".icon--close");
+const icon_open = document.querySelector(".icon--open");
+const nav = document.querySelector(".navigation__nav");
+const navigation_link = document.querySelectorAll(".navigation__link");
+const section = document.querySelector(".section-about");
+
+navigation_icon.addEventListener("click", toggleMenu);
+
+
+function toggleMenu () {
+  if (nav.classList.contains("showMenu")) {
+    nav.classList.remove("showMenu");
+    icon_close.style.display = "none";
+    icon_open.style.display = "block";
+    section.classList.remove("overlay");
+  } else {
+    nav.classList.add("showMenu");
+    icon_close.style.display = "block";
+    icon_open.style.display = "none";
+    section.classList.add("overlay");
+  }
+}
+
+navigation_link.forEach( 
+  function(link) { 
+    link.addEventListener("click", toggleMenu);
+  }
+)
+
+
+
